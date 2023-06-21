d279397935524708f49ed6079edefd2e7d116cdd
Hoàng Văn Hiệu <104782203+hoanghieu2001@users.noreply.github.com>
2023-06-21 19:43:58 +0700

    fdf

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,19 @@ function App() {
   const products = {
     1: {
       id: 1,
-      title: "Mediolas",
+      title: "chó",
       thumbnail: "https://tingenz.com/wp-content/uploads/2023/01/tho-bay-mau-cute-4-min.jpg",
       introduct: "Thịt thỏ",
     },
     2: {
       id: 2,
-      title: "Piltover",
+      title: "mèo",
       thumbnail: "https://i.pinimg.com/originals/d8/f8/74/d8f874e43f6054dc8f02447ccd4e7bdb.jpg",
       introduct: "Thỏ ngu",
     },
     3: {
       id: 3,
-      title: "SlyderKing",
+      title: "thầy ba gà",
       thumbnail: "https://i.pinimg.com/736x/9d/3c/a5/9d3ca5906d602c943f66e9b0d629f82f.jpg",
       introduct: "Chạy code đi",
     },
